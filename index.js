dd25083b1968c189831d0b2daa841f8a98a5c384
Caren Keyes <cejkeyes@gmail.com>
2018-11-17 14:45:08 -0800

    add hover interaction for color wheel

---
+++ index.js
@@ -0,0 +1,65 @@
+function handleColorIcons(){
+    $('.js-icon-red'). on("mouseover", function(e){
+        $('.color-wheel-red').removeClass('display-none');
+        hideFullWheel()
+    })
+    $('.js-icon-red').on("mouseleave", function(e){
+        $('.color-wheel-red').addClass('display-none');
+        displayFullWheel();
+    })
+
+    $('.js-icon-orange'). on("mouseover", function(e){
+        $('.color-wheel-orange').removeClass('display-none');
+        hideFullWheel()
+    })
+    $('.js-icon-orange').on("mouseleave", function(e){
+        $('.color-wheel-orange').addClass('display-none');
+        displayFullWheel();
+    })
+
+    $('.js-icon-yellow'). on("mouseover", function(e){
+        $('.color-wheel-yellow').removeClass('display-none');
+        hideFullWheel()
+    })
+    $('.js-icon-yellow').on("mouseleave", function(e){
+        $('.color-wheel-yellow').addClass('display-none');
+        displayFullWheel();
+    })
+
+    $('.js-icon-green'). on("mouseover", function(e){
+        $('.color-wheel-green').removeClass('display-none');
+        hideFullWheel()
+    })
+    $('.js-icon-green').on("mouseleave", function(e){
+        $('.color-wheel-green').addClass('display-none');
+        displayFullWheel();
+    })
+
+    $('.js-icon-blue'). on("mouseover", function(e){
+        $('.color-wheel-blue').removeClass('display-none');
+        hideFullWheel()
+    })
+    $('.js-icon-blue').on("mouseleave", function(e){
+        $('.color-wheel-blue').addClass('display-none');
+        displayFullWheel();
+    })
+
+    $('.js-icon-purple'). on("mouseover", function(e){
+        $('.color-wheel-purple').removeClass('display-none');
+        hideFullWheel()
+    })
+    $('.js-icon-purple').on("mouseleave", function(e){
+        $('.color-wheel-purple').addClass('display-none');
+        displayFullWheel();
+    })
+}
+
+function hideFullWheel(){
+    $('.color-wheel-full').addClass('display-none');
+}
+
+function displayFullWheel(){
+    $('.color-wheel-full').removeClass('display-none');
+}
+
+handleColorIcons();
