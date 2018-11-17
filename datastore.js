9a08280c5418cc336206eff372bd2856bfc0fda5
Caren Keyes <cejkeyes@gmail.com>
2018-11-17 15:34:35 -0800

    Add dummy location data

---
+++ datastore.js
@@ -0,0 +1,57 @@
+const locationSTORE = [
+    {
+        Name: "Sole Intensity Dance Academy",
+        ImageURL: "https://www.soleintensitydance.com/uploads/1/1/9/1/119177682/studio4-min-min_orig.jpg",
+        Reviews: [
+            {
+                Name: "Happy Dancer",
+                Review: "So glad to have found a place to work on my new routine. It's nice to practice uninterrupted"
+            },
+            {
+                Name: "Aerial Athlete",
+                Review: "Thank goodness I can practice catching that dumb ring without anyone watching!"
+            }
+        ],
+        Description: "A small studio in North Vegas. Full-wall mirror, a heavy-duty floor suitable for tap and equiptment such as ballet barres and yoga balls",
+        Location: {
+            Latitude: "36.276530",
+            Longitude: "-115.179710",
+        },
+        Category: "Performing Arts",
+        Rate: "$27/hr"
+    },
+    {
+        Name: "Sin City Stages",
+        ImageURL: "http://sincitystages.com/wp-content/uploads/2018/03/sets16-1600x1067.jpg",
+        Review: [
+            {
+                Name: "Starving artist",
+                Reviews: "I love this place because I can sit in the green room and pretend like I have a job"
+            },
+        ],
+        Description: "Includes stage lighting, access to props, a space green room, makeup area, break room, bathrooms, wifi and 5 parking spaces",
+        Location: {
+            Latitude: "36.1699",
+            Longitude: "-115.1398",
+        },
+        Category: "Performing Arts",
+        Rate: "$800/ 8 hr day"
+    },
+    {
+        Name: "Vegas Photo Studio Rentals",
+        ImageURL: "http://vegasphotostudiorentals.com/wp-content/themes/ThisWay/includes/timthumb.php?src=http://vegasphotostudiorentals.com/wp-content/uploads/2012/04/bg_0BA4gANmHHJXZW6O.jpg&w=120&h=80&zc=1&q=100",
+        Review: [
+            {
+                Name: "Annoyed User",
+                Review: "This would be a great place except that the website is really horrible. You should definitely book through artists connect"
+            }
+        ],
+        Description: "The studio offers three main shooting areas to accommodate commercial clients: A 360 sq ft cyclorama / infinity cove, a 16’ wide x 10’ tall chroma key video green screen, and a 12 x 8 ft rolling paint-able wall for room arrangements and scene set ups. The studio space is 1300 sq ft, unobstructed and can be used in any way imaginable. A 10’ x 10’ roll up door faces East, providing a refreshing natural light, allows for easy loading and unloading, the ability to shoot vehicles, or to accommodate large sets and props. The Studio is available to other working professional and student artists at low hourly rental prices.",
+        Location: {
+            Latitude: "36.13192",
+            Longitude: "-115.192420",
+        },
+        Category: "Digital Art",
+        Rate: "Performing Arts"
+    },
+]
