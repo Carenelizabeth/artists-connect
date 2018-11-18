3cf30d6f857535a87f1e1190882c185a8b46c6ba
Caren Keyes <cejkeyes@gmail.com>
2018-11-17 19:38:51 -0800

    changed index.html file

--- datastore.js
+++ datastore.js
@@ -37,18 +37,19 @@ const locationSTORE = [
         Rate: "$800/ 8 hr day"},
     {
         Name: "Vegas Photo Studio Rentals",
-        ImageURL: "http://vegasphotostudiorentals.com/wp-content/themes/ThisWay/includes/timthumb.php?src=http://vegasphotostudiorentals.com/wp-content/uploads/2012/04/bg_0BA4gANmHHJXZW6O.jpg&w=120&h=80&zc=1&q=100",
+        ImageURL: "http://vegasphotostudiorentals.com/wp-content/uploads/2012/03/bg_NeAhdcZO3UmgNVab.jpg",
         Reviews: [
             {
                 Name: "Annoyed User",
                 Review: "This would be a great place except that the website is really horrible. You should definitely book through artists connect"
             }
         ],
-        Description: "The studio offers three main shooting areas to accommodate commercial clients: A 360 sq ft cyclorama / infinity cove, a 16’ wide x 10’ tall chroma key video green screen, and a 12 x 8 ft rolling paint-able wall for room arrangements and scene set ups. The studio space is 1300 sq ft, unobstructed and can be used in any way imaginable. A 10’ x 10’ roll up door faces East, providing a refreshing natural light, allows for easy loading and unloading, the ability to shoot vehicles, or to accommodate large sets and props. The Studio is available to other working professional and student artists at low hourly rental prices.",
+        Description: "The studio offers three main shooting areas to accommodate commercial clients: A 360 sq ft cyclorama / infinity cove, a 16’ wide x 10’ tall chroma key video green screen, and a 12 x 8 ft rolling paint-able wall for room arrangements and scene set ups.",
         Location: {
             Latitude: "36.13192",
             Longitude: "-115.192420",
         },
         Category: "Digital Art",
-        Rate: "Performing Arts"},
+        Rate: "$120/2hrs",
+    }
 ]
